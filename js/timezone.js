89695934f064a89e97663e732a2a8c5d0be6ae0e
royhan31 <izzapark34@gmail.com>
2020-03-27 08:38:24 +0700

    initial commit

---
+++ js/timezone.js
@@ -0,0 +1,2 @@
+const m = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+const h = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat","Sabtu"];
